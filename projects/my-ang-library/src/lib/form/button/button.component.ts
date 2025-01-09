2cd18f01c67c279bc53a6651ec58eb2f0f25fb51
kamdem arielle <arielle.kamdem@pether.io>
2025-01-09 23:05:14 +0100

    tried creating a module of components

---
+++ projects/my-ang-library/src/lib/form/button/button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'arielle-button',
+  standalone: false,
+  
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+}
---
+++ projects/my-ang-library/src/lib/form/input/input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'arielle-input',
+  standalone: false,
+  
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.css'
+})
+export class InputComponent {
+
+}
