9dd1f5dc7b0785e7d0657294b3cef4c5fa037cd9
kamdem arielle <arielle.kamdem@pether.io>
2025-01-12 00:28:10 +0100

    first stable version of the button component with ripple effect

--- projects/my-ang-library/src/lib/form/button/button.component.ts
+++ projects/my-ang-library/src/lib/form/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, Input, input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef,Input, input, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'arielle-form-button',
@@ -7,7 +7,8 @@ import { Component, HostBinding, Input, input } from '@angular/core';
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
-export class ButtonComponent {
+
+export class ButtonComponent implements AfterViewInit {
 @Input() label = 'Accept';
 @Input() width = '190px';
 @Input() primary: boolean = false;
@@ -15,26 +16,63 @@ export class ButtonComponent {
 @Input() callModal: boolean = false;
 @Input() enableModal: boolean = false;
 @Input() primary2: boolean = false;
+@Input() rippleEffect : boolean = true;
+displayStyle: string = 'none'; 
+isRippleActive:boolean = false;
+@ViewChild('FormButtonRef', { static: true }) buttonRef!: ElementRef; // Reference to the button element
+@ViewChild('ButtonSpanRef', { static: true }) buttonSpanRef!: ElementRef; // Reference to the button element
 
-// Add `HostBinding` for attributes
-@HostBinding('attr.primary') get isRaised() {
-  return this.primary ? '' : null;
+
+constructor(private renderer: Renderer2) {}
+
+ngAfterViewInit() {
+
+  // Add attributes to the button component depending on the input values
+
+  if (this.primary) {
+    this.renderer.setAttribute(this.buttonRef.nativeElement, 'primary', '');
+  } 
+  if (this.alert) {
+    this.renderer.setAttribute(this.buttonRef.nativeElement, 'alert', '');
+  }
+  if (this.callModal) {
+    this.renderer.setAttribute(this.buttonRef.nativeElement, 'callModal', '');
+  }
+  if (this.enableModal) {
+    this.renderer.setAttribute(this.buttonRef.nativeElement, 'enableModal', '');
+  }
+  if (this.primary2) {
+    this.renderer.setAttribute(this.buttonRef.nativeElement, 'primary2', '');
+  }
+ 
 }
 
-@HostBinding('attr.alert') get isFlat() {
-  return this.alert ? '' : null;
-}
+addRippleEffect(e:any){
+  if (this.rippleEffect) {
+  if (this.isRippleActive) return; 
 
-@HostBinding('attr.call-modal') get isStroked() {
-  return this.callModal ? '' : null;
-}
+    this.isRippleActive = true;
+    // const ripple = document.createElement('span');
+    // ripple.classList.add('blocki');
+    // Get the horizontal position of mouse relative to the button
+    let x = e.clientX - e.currentTarget.offsetLeft;
 
-@HostBinding('attr.enable-modal') get isWarn() {
-  return this.enableModal ? '' : null;
-}
+    // Get the vertical position of the mouse relative to the button
+    let y = e.clientY - e.currentTarget.offsetTop;
 
-@HostBinding('attr.primary2') get isDisabled() {
-  return this.primary2 ? true : null;
+    this.renderer.setStyle(this.buttonSpanRef.nativeElement, 'left', `${x}px` );
+    this.renderer.setStyle(this.buttonSpanRef.nativeElement, 'top', `${y}px` );
+
+    this.displayStyle = 'inline'
+    // ripple.style.left = `${x}px`;
+    // ripple.style.top = `${y}px`;
+  //   this.buttonRef.nativeElement.appendChild(ripple);
+    setTimeout(() => {
+      this.displayStyle = 'none'
+      this.isRippleActive = false;
+    }, 1000);
+
+}
 }
 
 
