609d5ae0a3505542d73689ffd05fddcec7c49dd9
kamdem arielle <arielle.kamdem@pether.io>
2025-01-24 23:35:00 +0100

    created blocck for dynamic sidebar links and added some style to it v1

--- projects/my-ang-library/src/lib/arielle-sidenav/drawer/drawer.component.ts
+++ projects/my-ang-library/src/lib/arielle-sidenav/drawer/drawer.component.ts
@@ -6,14 +6,15 @@ import {
   ViewChild,
   viewChild,
 } from '@angular/core';
-import { faAnglesRight,faAnglesLeft } from '@fortawesome/free-solid-svg-icons';
+import { faAnglesRight,faAnglesLeft,} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {NgClass} from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 
 @Component({
   selector: 'arielle-drawer',
-  imports: [NgClass,FontAwesomeModule ],
+  imports: [NgClass,FontAwesomeModule,RouterLink,RouterLinkActive ],
   templateUrl: './drawer.component.html',
   styleUrl: './drawer.component.scss',
 })
@@ -22,17 +23,57 @@ export class DrawerComponent {
   @Input() mode = 'side';
   @Input() width!: number;
   @Input() default!: boolean;
+  @Input() navBarLinks:any = [];
   collapsed!: boolean;
   iconCollapse!: boolean;
   faAnglesRight = faAnglesRight;
   faAnglesLeft = faAnglesLeft;
+  classActive :any;
+  showMenu:string='';
   @ViewChild('drawerContainer', { static: false }) drawerContainer!: ElementRef;
 
   constructor(private renderer: Renderer2, private el: ElementRef) {}
 
+  ngOnInit() {
+    this.navBarLinks = [
+      {
+        name: 'Home',
+        route: 'home',
+        icon: 'ic_home',
+        sublinks: []
+      },
+      {
+        name: 'Profile',
+        route: 'profile',
+        icon: 'ic_profile',
+        sublinks: [
+          { name: 'Settings',route: 'settings', icon: 'ic_settings' },
+          { name: 'Logout',route: 'logout', icon: 'ic_logout' }
+        ]
+      },
+      // Add more links as needed
+    ];
+
+
+    if (window.innerWidth>991) {
+      console.log("button left ")
+      this.iconCollapse=false;
+    }else{
+      console.log("button right")
+      this.iconCollapse=true
+    }
+  }
+
+
   ngAfterViewInit() {
     if (this.position == 'right') {
       this.renderer.setAttribute(this.el.nativeElement, 'positionRight', '');
+      this.faAnglesRight = faAnglesLeft;
+      this.faAnglesLeft = faAnglesRight;
+      const targetElement = document.querySelector('.main-layout');
+      if (targetElement) {
+        this.renderer.addClass(targetElement, 'highlight'); // Add the class to the target component
+      }
     } else {
       this.renderer.setAttribute(this.el.nativeElement, 'positionLeft', '');
     }
@@ -45,15 +86,7 @@ export class DrawerComponent {
     }
   }
 
-  ngOnInit() {
-    if (window.innerWidth>991) {
-      console.log("button left ")
-      this.iconCollapse=false;
-    }else{
-      console.log("button right")
-      this.iconCollapse=true
-    }
-  }
+  
 
   toggleCollapsed() {
     var sidebar = document.querySelector('.drawer-container');
@@ -99,5 +132,36 @@ export class DrawerComponent {
     sidebar!.classList.remove('openSideBar');
     sidebar!.classList.remove('closeSideBarLargeScreen');
     collapsing_shadow!.classList.remove('openSideBar');
+  }
+
+  onActive(event: Event) {
+    let element = event.target as HTMLElement;
+    var sidebar =document.querySelector(".drawer-container");
+    if (window.innerWidth < 991 && !sidebar!.classList.contains("openSideBar")) {
+      this.toggleCollapsed()
+    }
+    if (window.innerWidth > 991 && sidebar!.classList.contains("closeSideBarLargeScreen")) {
+      this.toggleCollapsed()
+    }
+    //ElementRef
+    if (this.classActive) {
+      // last activated menu
+      if (this.classActive === element) {
+        this.classActive!.classList.toggle("active"); //toggle off
+        this.classActive = null;
+        return; //exit
+      }
+      this.classActive.classList.toggle("active"); //toggle off last activated menu
+    }
+    element.classList.toggle("active"); // activated new menu
+    this.classActive = element;
+  }
+
+  addExpandClass(element: any) {
+    if (element === this.showMenu) {
+      this.showMenu = "0";
+    } else {
+      this.showMenu = element;
+    }
   }
 }
